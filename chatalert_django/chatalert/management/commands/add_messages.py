f2ee5c3cbc6a1433f6d520567a64022e1873f70b
Zachary McCord <zjmccord@gmail.com>
2016-01-23 21:23:42 -0500

    Working on creating skeleton Django project for UI

---
+++ chatalert_django/chatalert/management/commands/add_messages.py
@@ -0,0 +1,85 @@
+"""
+Management command to import a CSV file into the database. Used to introduce
+new data to the system to be labeled and displayed.
+"""
+
+from __future__ import print_function
+
+import csv
+import datetime
+import logging
+import re
+import traceback
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from ...models import Message
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    can_import_settings = True
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_files', nargs="+")
+
+    def handle(self, *args, **options):
+        for file_name in options['csv_files']:
+            reader = csv.DictReader(open(file_name))
+            for row in reader:
+                self.import_row(row)
+
+    @classmethod
+    def clean_newlines(cls, row):
+        " Remove newlines or literal backslash-N sequences from input "
+        result = dict()
+        for key in row:
+            result[key] = re.sub(r'\\n|\n', '', row[key], flags=re.IGNORECASE)
+        return result
+
+    @classmethod
+    def parse_date(cls, date_text):
+        patterns = ["%m/%d/%y %H:%M",
+                    "%Y-%m-%d %H:%M:%S",
+                    "%m/%d/%Y %H:%M:%S",
+                    "%m/%d/%Y %H:%M"]
+        for pattern in patterns:
+            try:
+                return datetime.datetime.strptime(date_text, pattern)
+            except ValueError:
+                pass
+        raise ValueError("Date text %r didn't match any of the patterns %s" %
+                         (date_text, patterns))
+                
+
+    @classmethod
+    def import_row(cls, row):
+        " Import a single row into the database "
+        row = cls.clean_newlines(row)
+        try:
+            with transaction.atomic():
+                message, _ = Message.objects.get_or_create(guid=row["guid"])
+                message.creation_datetime = \
+                    cls.parse_date(row["creation_date"])
+                message.message_type = row["type"]
+                message.source = row["source"]
+                message.url = row["url"]
+                message.text = row["text"]
+                message.author = row["author"]
+                message.author_age = row["author_age"]
+                message.city_state = row["city_state"]
+                message.lat = row["lat"] or 0
+                message.lng = row["lng"] or 0
+                message.phone_number = row["phone_number"]
+                message.post_id = row["post_id"]                
+                message.clean()
+                message.save()
+
+                logger.info("Imported message %s", row.get("guid"))
+        except:
+            logger.error("Failed to import message %s: %s",
+                         row.get("guid"), traceback.format_exc())
+            raise
---
+++ chatalert_django/chatalert/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Message(models.Model):
+
+    LABEL_NO_LABEL = -1
+    LABEL_NO_INTEREST = 0
+    LABEL_INTEREST = 1
+    LABEL_CRITICAL = 2
+
+    LABEL_CHOICES = [(LABEL_NO_LABEL, "No Label"),
+                     (LABEL_NO_INTEREST, "Review"),
+                     (LABEL_INTEREST, "LE Interest"),
+                     (LABEL_CRITICAL, "Urgent!")]
+
+    guid = models.TextField(unique=True)
+    creation_datetime = models.DateTimeField(null=True, blank=True)
+    message_type = models.TextField(blank=True)
+    source = models.TextField(blank=True)
+    message_url = models.TextField(blank=True)
+    text = models.TextField(blank=True)
+    author = models.TextField(blank=True)
+    author_age = models.IntegerField(null=True, blank=True)
+    city_state = models.TextField(blank=True)
+    lat = models.FloatField(null=True, blank=True)
+    lng = models.FloatField(null=True, blank=True)
+    phone_number = models.TextField(blank=True)
+    post_id = models.TextField(blank=True)
+    # What do we do about timezone fields?
+
+    label_auto = models.IntegerField(choices=LABEL_CHOICES,
+                                     default=LABEL_NO_LABEL)
+    label_user = models.IntegerField(choices=LABEL_CHOICES,
+                                     default=LABEL_NO_LABEL)
